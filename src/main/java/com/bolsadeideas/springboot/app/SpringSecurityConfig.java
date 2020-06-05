ee3ae9d73495ac0e88ffd5cb4db13c899b1f4eae
git config --global user.name devsmn <simont98y@gmail.com>
2020-06-05 18:15:08 -0500

    Verificando roles desde el controlador SimpleGrantedAuthority

--- src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.bolsadeideas.springboot.app.auth.handler.LoginSuccessHandler;
 
+
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -24,16 +27,16 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar")
 		.permitAll()
-				.antMatchers("/ver/**")
-				.hasAnyRole("USER")
-				.antMatchers("/uploads/**")
-				.hasAnyRole("USER")
-				.antMatchers("/form/**")
+				/*.antMatchers("/ver/**")
+				.hasAnyRole("USER")*/
+				/*.antMatchers("/uploads/**")
+				.hasAnyRole("USER")*/
+				/*.antMatchers("/form/**")
 				.hasAnyRole("ADMIN")
 				.antMatchers("/eliminar/**")
 				.hasAnyRole("ADMIN")
 				.antMatchers("/factura/**")
-				.hasAnyRole("ADMIN")
+				.hasAnyRole("ADMIN")*/
 				.anyRequest()
 				.authenticated()
 				.and()
